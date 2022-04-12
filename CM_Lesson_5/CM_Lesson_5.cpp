f65155977b31f690e4cba4876431541460ba576b
petr-rusinov <petr-rusinov@yandex.ru>
2022-04-12 09:46:50 +0300

    function uniqueWords added

--- CM_Lesson_5/CM_Lesson_5.cpp
+++ CM_Lesson_5/CM_Lesson_5.cpp
@@ -1,5 +1,23 @@
 ﻿
 #include <iostream>
+#include <iterator>
+#include <set>
+
+using namespace std;
+
+template <class InputIt, class T>
+void uniqueWords(InputIt first, InputIt last)
+{
+    set<T> tempSet;
+
+    for (; first != last; ++first)
+    {
+        tempSet.insert(*first);
+    }
+
+    copy(tempSet.begin(), tempSet.end(), ostream_iterator<T>(cout, " "));
+}
+
 
 int main()
 {
